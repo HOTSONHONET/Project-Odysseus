8f1d7362bdb1f5ea174064fc3f26ea8e40b9b36b
HOTSONHONET <hotsonhonet@gmail.com>
2022-10-22 18:15:39 +0530

    added background animation

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -1,11 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function SignUp() {
-  return (
-    <div className="d-flex flex-row my-auto justify-content-center align-items-center">
-      <div className="form">
-        writing
-      </div>
-    </div>
-  )
+	const [name, update_name] = useState('');
+	const [org_name, update_org_name] = useState('');
+	const [email, update_email] = useState('');
+	const [password, update_password] = useState('');
+	const [submitClicked, update_submitClicked] = useState(false);
+
+	const handleSubmit = () => {
+		update_submitClicked(true);
+	}
+
+	return (
+		<div className="formdiv">
+			<div className="form formbg p-5 d-flex flex-column justify-content-center">
+				<div className="flex justify-content-center"><h3>Sign Up</h3></div>
+				<div className="row m-3">
+					<div className="col col-sm-4">
+						Name
+					</div>
+					<div className="col col-sm-8 ">
+						<input for="name" id="name" type="text" onChange={(e) => { update_name(e.target.value) }} disabled={submitClicked} />
+					</div>
+				</div>
+				<div className="row m-3">
+					<div className="col col-sm-4">
+						Organization
+					</div>
+					<div className="col col-sm-8 ">
+						<input for="name" id="name" type="text" onChange={(e) => { update_org_name(e.target.value) }} disabled={submitClicked} />
+					</div>
+				</div>
+				<div className="row m-3">
+					<div className="col col-sm-4">
+						Email
+					</div>
+					<div className="col col-sm-8 ">
+						<input for="name" id="name" type="email" onChange={(e) => { update_email(e.target.value) }} disabled={submitClicked} />
+					</div>
+				</div>
+				<div className="row m-3">
+					<div className="col col-sm-4">
+						Password
+					</div>
+					<div className="col col-sm-8 ">
+						<input for="name" id="name" type="password" onChange={(e) => { update_password(e.target.value) }} disabled={submitClicked} />
+					</div>
+				</div>
+				<button className="btn btn-primary btn-sm" type="submit" onClick={() => { handleSubmit() }}>Submit</button>
+
+			</div>
+		</div>
+	)
 }
